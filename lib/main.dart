ab16b88303b48485813057900631db91ec250899
spd <dattasoumyaprakash@gmail.com>
2023-10-31 15:14:44 +0800

    refactored code

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
-import 'dart:developer' as developer;
 import "./todo.dart";
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  const keyApplicationId = String.fromEnvironment('KeyApplicationId');
-  const keyClientKey = String.fromEnvironment('KeyClientKey');
-  const keyParseServerUrl = String.fromEnvironment('KeyParseServerUrl');
 
-  await Parse().initialize(keyApplicationId, keyParseServerUrl,
-      clientKey: keyClientKey, autoSendSessionId: true, debug: true);
-
-  final ParseResponse apiResponse =
-      await QueryBuilder<ParseObject>(ParseObject('todo')).query();
-  if (apiResponse.success && apiResponse.results != null) {
-    // Let's show the results
-    for (var o in apiResponse.results!) {
-      developer.log((o as ParseObject).toString());
-    }
-  }
+  await initParseSDK();
 
   runApp(const MyApp());
 }
@@ -32,4 +18,13 @@ class MyApp extends StatelessWidget {
     // return MaterialApp(title: "Startup name generator", home: _RandomWords());
     return const MaterialApp(title: "Todo App", home: Todo());
   }
+}
+
+Future<Parse> initParseSDK() async {
+  const keyApplicationId = String.fromEnvironment('KeyApplicationId');
+  const keyClientKey = String.fromEnvironment('KeyClientKey');
+  const keyParseServerUrl = String.fromEnvironment('KeyParseServerUrl');
+
+  return await Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, autoSendSessionId: true, debug: true);
 }
