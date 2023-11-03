6c16eddd41a0b31cbb788761c518df1a036f22b2
spd <dattasoumyaprakash@gmail.com>
2023-11-03 12:37:47 +0800

    added completed section, moved created todo button

--- lib/todo.dart
+++ lib/todo.dart
@@ -131,108 +131,130 @@ class TodoListViewState extends State<TodoListView> {
     final Size size = MediaQuery.of(context).size;
     developer.log("loading: $appBarLoading");
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Today's tasks"),
-          backgroundColor: const Color.fromARGB(255, 0, 18, 182),
-          bottom: appBarLoading
-              ? PreferredSize(
-                  preferredSize: Size(size.width, 0),
-                  child: const LinearProgressIndicator(
-                    backgroundColor: Colors.red,
-                  ),
-                )
-              : null,
-        ),
-        body: Column(children: <Widget>[
-          Container(
-              padding: const EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
-              child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: <Widget>[
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor: const Color.fromARGB(255, 0, 18, 182),
-                    ),
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              scrollable: true,
-                              title: const Text("Add todo"),
-                              content: Padding(
-                                padding: const EdgeInsets.all(16),
-                                child: Form(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    children: <Widget>[
-                                      TextFormField(
-                                        autocorrect: true,
-                                        textCapitalization: TextCapitalization.sentences,
-                                        controller: todoTitleController,
-                                        decoration: const InputDecoration(
-                                            labelText: "todo title",
-                                            labelStyle: TextStyle(color: Color.fromARGB(255, 0, 18, 182))),
-                                      ),
-                                      TextFormField(
-                                        autocorrect: true,
-                                        textCapitalization: TextCapitalization.sentences,
-                                        controller: todoDescriptionController,
-                                        decoration: const InputDecoration(
-                                            labelText: "todo description",
-                                            labelStyle: TextStyle(color: Color.fromARGB(255, 0, 18, 182))),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              actions: [
-                                ElevatedButton(
-                                  onPressed: () async {
-                                    await createEntry(context, () {
-                                      if (!mounted) {
-                                        return;
-                                      }
-                                      Navigator.of(context).pop();
-                                    });
-                                  },
-                                  child: const Text("submit"),
-                                ),
-                              ],
-                            );
-                          });
-                      // createEntry();
-                    },
-                    child: const Text("ADD")),
-              ])),
-          Expanded(
-              child: FutureBuilder<List<TodoEntry>>(
-                  future: todos,
-                  builder: (ctx, snapshot) {
-                    if (listLoading &&
-                        (snapshot.connectionState == ConnectionState.none ||
-                            snapshot.connectionState == ConnectionState.waiting)) {
+      appBar: AppBar(
+        title: const Text("Today's tasks"),
+        backgroundColor: const Color.fromARGB(255, 0, 18, 182),
+        bottom: appBarLoading
+            ? PreferredSize(
+                preferredSize: Size(size.width, 0),
+                child: const LinearProgressIndicator(
+                  backgroundColor: Colors.red,
+                ),
+              )
+            : null,
+      ),
+      body: Column(children: <Widget>[
+        Expanded(
+            child: FutureBuilder<List<TodoEntry>>(
+                future: todos,
+                builder: (ctx, snapshot) {
+                  if (listLoading &&
+                      (snapshot.connectionState == ConnectionState.none ||
+                          snapshot.connectionState == ConnectionState.waiting)) {
+                    return const Center(
+                      child: SizedBox(width: 100, height: 100, child: CircularProgressIndicator()),
+                    );
+                  } else {
+                    if (snapshot.hasError) {
                       return const Center(
-                        child: SizedBox(width: 100, height: 100, child: CircularProgressIndicator()),
+                        child: Text("Error while fetching todo list"),
+                      );
+                    } else if (!snapshot.hasData) {
+                      return const Center(
+                        child: Text("No todo found"),
                       );
                     } else {
-                      if (snapshot.hasError) {
-                        return const Center(
-                          child: Text("Error while fetching todo list"),
-                        );
-                      } else if (!snapshot.hasData) {
-                        return const Center(
-                          child: Text("No todo found"),
-                        );
-                      } else {
-                        return TaskListEntryView(
-                          data: snapshot.data!,
-                          toggleTodoEntryDone: toggleTodoEntryDone,
-                          deleteEntry: deleteEntry,
-                        );
-                      }
+                      return ListView(
+                        children: <Widget>[
+                          TaskListEntryView(
+                            data: snapshot.data!.where((e) => e.done == false).toList(),
+                            toggleTodoEntryDone: toggleTodoEntryDone,
+                            deleteEntry: deleteEntry,
+                          ),
+                          const DividerWithText(text: "COMPLETED"),
+                          TaskListEntryView(
+                            data: snapshot.data!.where((e) => e.done == true).toList(),
+                            toggleTodoEntryDone: toggleTodoEntryDone,
+                            deleteEntry: deleteEntry,
+                          ),
+                        ],
+                      );
                     }
-                  }))
-        ]));
+                  }
+                }))
+      ]),
+      floatingActionButton: FloatingActionButton(
+        tooltip: "+",
+        child: Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  scrollable: true,
+                  title: const Text("Add todo"),
+                  content: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Form(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          TextFormField(
+                            autocorrect: true,
+                            textCapitalization: TextCapitalization.sentences,
+                            controller: todoTitleController,
+                            decoration: const InputDecoration(
+                                labelText: "todo title", labelStyle: TextStyle(color: Color.fromARGB(255, 0, 18, 182))),
+                          ),
+                          TextFormField(
+                            autocorrect: true,
+                            textCapitalization: TextCapitalization.sentences,
+                            controller: todoDescriptionController,
+                            decoration: const InputDecoration(
+                                labelText: "todo description",
+                                labelStyle: TextStyle(color: Color.fromARGB(255, 0, 18, 182))),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () async {
+                        await createEntry(context, () {
+                          if (!mounted) {
+                            return;
+                          }
+                          Navigator.of(context).pop();
+                        });
+                      },
+                      child: const Text("submit"),
+                    ),
+                  ],
+                );
+              });
+          // createEntry();
+        },
+      ),
+    );
+  }
+}
+
+class DividerWithText extends StatelessWidget {
+  const DividerWithText({
+    super.key,
+    required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: <Widget>[
+      const Expanded(child: Divider()),
+      Text(text),
+      const Expanded(child: Divider()),
+    ]);
   }
 }
 
@@ -253,9 +275,18 @@ class TaskListEntryViewState extends State<TaskListEntryView> {
     borderRadius: BorderRadius.circular(10),
   );
 
+  TextDecoration getTextDecoration(bool done) {
+    if (done == true) {
+      return TextDecoration.lineThrough;
+    }
+    return TextDecoration.none;
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
         itemCount: widget.data.length,
         padding: const EdgeInsets.all(16),
         itemBuilder: (context, index) {
@@ -289,11 +320,11 @@ class TaskListEntryViewState extends State<TaskListEntryView> {
                     child: ListTile(
                       title: Text(
                         "$title - $sinceStr",
-                        style: const TextStyle(color: Colors.white),
+                        style: TextStyle(color: Colors.white, decoration: getTextDecoration(widget.data[index].done)),
                       ),
                       subtitle: Text(
                         description,
-                        style: const TextStyle(color: Colors.white),
+                        style: TextStyle(color: Colors.white, decoration: getTextDecoration(widget.data[index].done)),
                       ),
                     ),
                   ),
